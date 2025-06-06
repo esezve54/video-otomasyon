8f8f6f922b78e1709d59ddaa66f2867f0d90912e
esezve54 <serkan.cimen.738@gmail.com>
2025-06-06 19:23:10 +0300

    Create app.py

---
+++ app.py
@@ -0,0 +1,18 @@
+video-otomasyon/
+│
+├── app.py
+├── config.py
+├── models.py
+├── requirements.txt
+├── Procfile
+├── Dockerfile
+│
+├── temp_media/
+│   └── .gitkeep
+│
+├── logs/
+│   └── .gitkeep
+│
+└── templates/
+    ├── base.html
+    └── admin.html
